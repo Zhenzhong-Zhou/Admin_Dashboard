53822c179f9d0d7fbcd38346dba1ea08c1564026
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-06 16:21:31 -0600

    Add userlist page

--- admin/src/pages/UserList/UserList.jsx
+++ admin/src/pages/UserList/UserList.jsx
@@ -1,8 +1,17 @@
+import {useState} from "react";
 import {DataGrid} from "@mui/x-data-grid";
+import {DeleteOutline} from "@material-ui/icons";
+import {Link} from "react-router-dom";
 import "./styles.css";
-import user from "../../assets/images/user2.png";
+import {userRows} from "../../boilerplateData";
 
 const UserList = () => {
+	const [data, setData] = useState(userRows);
+
+	const handleDelete = (id) => {
+		setData(data.filter(item => item.id !== id));
+	};
+
 	const columns = [
 		{ field: 'id', headerName: 'ID', width: 90 },
 		{
@@ -10,6 +19,14 @@ const UserList = () => {
 			headerName: 'Username',
 			width: 150,
 			editable: true,
+			renderCell: (params) => {
+				return (
+					<div className={"userListUser"}>
+						<img className={"userListImg"} src={params.row.avatar} alt={"Avatar"}/>
+						{params.row.username}
+					</div>
+				);
+			}
 		},
 		{
 			field: 'email',
@@ -28,16 +45,28 @@ const UserList = () => {
 			headerName: 'Transaction Volume',
 			width: 200,
 			editable: true,
+		},
+		{
+			field: "action",
+			headerName: "Action",
+			width: 150,
+			editable: true,
+			renderCell: (params) => {
+				return (
+					<>
+						<Link to={`/user/${params.row.id}`}>
+							<button className={"userListEdit"}>Edit</button>
+						</Link>
+						<DeleteOutline className={"userListDelete"} onClick={() => handleDelete(params.row.id)}/>
+					</>
+				);
+			}
 		}
-	];
-
-	const rows = [
-		{ id: 1, username: 'Jon Snow', avatar: user, email: "jon@gamil.com", status: "active", transaction: "$120.00",}
 	];
 
 	return (
 		<div className={"userList"}>
-			<DataGrid columns={columns} rows={rows} pageSize={5} checkboxSelection/>
+			<DataGrid columns={columns} rows={data} pageSize={8} checkboxSelection disableSelectionOnClick/>
 		</div>
 	);
 };
