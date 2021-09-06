d44d3d3df6d097c1841651c5c3eb045af3db18b1
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-06 01:54:27 -0600

    Add featuredInfo styles

--- admin/src/featurdInfo/FeaturedItem/Cost.jsx
+++ admin/src/featurdInfo/FeaturedItem/Cost.jsx
@@ -7,7 +7,7 @@ const Cost = () => {
 			<span className={"featuredTitle"}>Cost</span>
 			<div className={"featuredMoneyContainer"}>
 				<span className={"featuredMoney"}>$2,222</span>
-				<span className={"featuredMoneyRate"}>+2.6<ArrowUpward/></span>
+				<span className={"featuredMoneyRate"}>+2.6<ArrowUpward className={"featuredIcon"}/></span>
 			</div>
 			<span className={"featuredSub"}>Compared to last month</span>
 		</div>
--- admin/src/featurdInfo/FeaturedItem/Sale.jsx
+++ admin/src/featurdInfo/FeaturedItem/Sale.jsx
@@ -7,7 +7,7 @@ const Sale = () => {
 			<span className={"featuredTitle"}>Sales</span>
 			<div className={"featuredMoneyContainer"}>
 				<span className={"featuredMoney"}>$5,415</span>
-				<span className={"featuredMoneyRate"}>-1.4<ArrowDownward/></span>
+				<span className={"featuredMoneyRate"}>-1.4<ArrowDownward className={"featuredIcon negative"}/></span>
 			</div>
 			<span className={"featuredSub"}>Compared to last month</span>
 		</div>
