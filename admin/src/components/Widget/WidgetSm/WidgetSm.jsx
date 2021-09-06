516bcfbcbf1a04ea55a492e9c9abc2dc4daaf5a6
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-06 03:35:38 -0600

    Add widget small component

--- admin/src/components/Widget/WidgetSm/WidgetSm.jsx
+++ admin/src/components/Widget/WidgetSm/WidgetSm.jsx
@@ -1,9 +1,21 @@
 import "./styles.css";
+import user from "../../../assets/images/user.jpeg";
+import {Visibility} from "@material-ui/icons";
 
 const WidgetSm = () => {
 	return (
 		<div className={"WidgetSm"}>
-			WidgetSm
+			<span className={"WidgetSmTitle"}>New Join Members</span>
+			<ul className={"WidgetSmList"}>
+				<li className={"WidgetSmListItem"}>
+					<img src={user} alt={"Avatar"} className={"WidgetSmImg"}/>
+					<div className={"WidgetSmUser"}>
+						<span className={"WidgetSmUsername"}>Crystal Liu</span>
+						<span className={"WidgetSmUserTitle"}>Actress</span>
+					</div>
+					<button className={"WidgetSmButton"}><Visibility className={"WidgetSmIcon"}/>Display</button>
+				</li>
+			</ul>
 		</div>
 	);
 };
