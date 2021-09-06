7ccb32f2c2ccf56d5a3223fdb57a705efed90f3b
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-06 00:02:25 -0600

    Add navbar component

--- admin/src/components/NavBar/NavBar.jsx
+++ admin/src/components/NavBar/NavBar.jsx
@@ -1,7 +1,29 @@
+import {Language, NotificationsNone, Settings} from "@material-ui/icons";
+import "./styles.css";
+import avatar from "../../assets/images/avatar.png";
+
 const NavBar = () => {
 	return (
-		<div>
-			NavBar
+		<div className={"navbar"}>
+			<div className={"navbarWrapper"}>
+				<div className={"topLeft"}>
+					<span className={"logo"}>Admin</span>
+				</div>
+				<div className={"topRight"}>
+					<div className={"navbarIconContainer"}>
+						<NotificationsNone/>
+						<span className={"navbarIconBadge"}>2</span>
+					</div>
+					<div className={"navbarIconContainer"}>
+						<Language/>
+						<span className={"navbarIconBadge"}>2</span>
+					</div>
+					<div className={"navbarIconContainer"}>
+						<Settings/>
+					</div>
+					<img className={"topAvatar"} alt={"Avatar"} src={avatar}/>
+				</div>
+			</div>
 		</div>
 	);
 };
