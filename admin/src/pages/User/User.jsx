053281209ef370a42a056ed2be004dfc1b1ad2b2
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-06 18:00:01 -0600

    Add user page and separate it two parts

--- admin/src/pages/User/User.jsx
+++ admin/src/pages/User/User.jsx
@@ -1,9 +1,33 @@
 import "./styles.css";
+import user from "../../assets/images/user3.jpeg";
+import {UserDisplayBottom, UserUpdateForm} from "../index";
+import {Link} from "react-router-dom";
 
 const User = () => {
 	return (
 		<div className={"user"}>
-			User
+			<div className={"userTitleContainer"}>
+				<h1 className={"userTitle"}>Edit User</h1>
+				<Link to={"/newUser"}>
+					<button className={"userAddButton"}>Create</button>
+				</Link>
+			</div>
+			<div className={"userContainer"}>
+				<div className={"userDisplay"}>
+					<div className={"userDisplayTop"}>
+						<img src={user} alt={"Avatar"} className={"userDisplayImg"}/>
+						<div className={"userDisplayTopTitle"}>
+							<span className={"userDisplayUsername"}>Crystal Liu</span>
+							<span className={"userDisplayUserTitle"}>Actress</span>
+						</div>
+					</div>
+					<UserDisplayBottom/>
+				</div>
+				<div className={"userUpdate"}>
+					<span className={"userUpdateTitle"}>Edit</span>
+					<UserUpdateForm/>
+				</div>
+			</div>
 		</div>
 	);
 };
--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -1,3 +1,5 @@
 export {default as Home} from "./Home/Home";
 export {default as UserList} from "./UserList/UserList";
 export {default as User} from "./User/User";
+export {default as UserDisplayBottom} from "./User/UserDispayBottom/UserDisplayBottom";
+export {default as UserUpdateForm} from "./User/UserUpdateForm/UserUpdateForm";
