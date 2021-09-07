a6d8ccdff8f42dfbaef5374f70e29241bbf7070c
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-06 19:05:53 -0600

    Add sidebar links and styles

--- admin/src/components/SideBar/sidebarMenu/Dashboard.jsx
+++ admin/src/components/SideBar/sidebarMenu/Dashboard.jsx
@@ -1,12 +1,15 @@
 import {LineStyle, Timeline, TrendingUp} from "@material-ui/icons";
 import "./styles.css";
+import {Link} from "react-router-dom";
 
 const Dashboard = () => {
 	return (
 		<div className={"sidebarMenu"}>
 			<h3 className={"sidebarTitle"}>Dashboard</h3>
 			<ul className={"sidebarList"}>
-				<li className={"sidebarListItem active"}><LineStyle className={"sidebarIcon"}/>Home</li>
+				<Link to={"/"} className={"link"}>
+					<li className={"sidebarListItem active"}><LineStyle className={"sidebarIcon"}/>Home</li>
+				</Link>
 				<li className={"sidebarListItem"}><Timeline className={"sidebarIcon"}/>Analytics</li>
 				<li className={"sidebarListItem"}><TrendingUp className={"sidebarIcon"}/>Sales</li>
 			</ul>
