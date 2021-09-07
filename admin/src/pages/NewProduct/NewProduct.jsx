e11737f72c9fddcbf49b4de5e561725ee6a877fb
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-06 21:18:35 -0600

    Add newproduct page

--- admin/src/pages/NewProduct/NewProduct.jsx
+++ admin/src/pages/NewProduct/NewProduct.jsx
@@ -3,7 +3,29 @@ import "./styles.css";
 const NewProduct = () => {
 	return (
 		<div className={"newProduct"}>
-			New Product
+			<h1 className={"newProductTitle"}>New Product</h1>
+			<form className={"newProductForm"}>
+				<div className={"newProductItem"}>
+					<label>Image</label>
+					<input type={"file"} id={"file"}/>
+				</div>
+				<div className={"newProductItem"}>
+					<label>Name</label>
+					<input type={"text"} placeholder={"Apple Airpods"}/>
+				</div>
+				<div className={"newProductItem"}>
+					<label>Stock</label>
+					<input type={"text"} placeholder={"123"}/>
+				</div>
+				<div className={"newProductItem"}>
+					<label>Active</label>
+					<select name={"active"} id={"active"}>
+						<option value={"yes"}>Yes</option>
+						<option value={"no"}>No</option>
+					</select>
+				</div>
+				<button className={"newProductButton"}>Create</button>
+			</form>
 		</div>
 	);
 };
