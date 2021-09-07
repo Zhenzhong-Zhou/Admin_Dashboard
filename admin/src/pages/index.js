1404938a2a3321d1330de72540d05677de0882fb
Zhenzhong Zhou <bob.zhou@usask.ca>
2021-09-06 18:57:13 -0600

    Add newuser page

--- admin/src/pages/index.js
+++ admin/src/pages/index.js
@@ -4,3 +4,4 @@ export {default as User} from "./User/User";
 export {default as UserDisplayBottom} from "./User/UserDispayBottom/UserDisplayBottom";
 export {default as UserUpdateForm} from "./User/UserUpdateForm/UserUpdateForm";
 export {default as NewUser} from "./NewUser/NewUser";
+export {default as NewUserForm} from "./NewUser/NewUserForm/NewUserForm";
